89addc763033aabb73f13a86dcf62f27998b3ecb
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2021-04-18 15:29:04 -0500

    Reloj Digital
    Aplicación que muestra la hora en tiempo real.

---
+++ Reloj/js/app.dev.js
@@ -0,0 +1,14 @@
+
+const getHora = () => {
+const fecha = new Date
+const tiempo = {
+hora: fecha.getHours (),
+minuto: fecha.getMinutes(),
+segundo: fecha.getSeconds()
+}
+const ahora = `${tiempo.hora} : ${tiempo.minuto} : ${tiempo.segundo}`;
+    
+    document.querySelector (".reloj").innerHTML = ahora;
+}
+
+setInterval(() =>  getHora(),1000); 
